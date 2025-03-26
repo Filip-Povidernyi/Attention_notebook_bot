a25ddb3e07b0c43648f29ca1f4699518e7a91697
Oleksandr Romashko <alex.rmshk@gmail.com>
2025-03-26 22:18:07 +0100

    Update contacts/birthdays stub

--- src/contacts/birthdays.py
+++ src/contacts/birthdays.py
@@ -1,13 +1,20 @@
 def get_upcoming_birthdays():
+    """
+    Displays upcoming birthdays within a specified number of days from today.
+
+    This function prompts the user to enter a number of days to filter upcoming 
+    birthdays. If a valid number is provided, it displays birthdays within that 
+    range.
+
+    The user can leave the input blank to show all birthdays.
+    """
     print("Show all upcoming birthdays in days range from now.")
 
     while True:
-        cmd = input("Please, enter number of days (or leave it blank to show all birthdays): ")
+        user_input = input("Please, enter number of days (or leave it blank to show all birthdays): ")
         
-        try:
-            days_range = int(cmd)
-            print(f"Print birthdays in {days_range} days range")
-            return days_range
-        
-        except ValueError:
-          print("Invalid days value. Please try again.")
+        if user_input.isdigit():
+            days_range = int(user_input)
+            print(f"Print birthdays in \"{days_range}\" days range")
+        else:
+          print("Invalid non-number days value. Please try again.")
