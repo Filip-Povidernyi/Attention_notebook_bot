a4562dff00d8bd1b46f3d2e338c915468ce64371
Natalia Khodorova <icxodnik@gmail.com>
2025-03-27 21:00:07 +0100

    fix styling (#39)
    * add parameter to notes
    
    * fix naming
    
    * append fix styles

--- src/notes/classes/note.py
+++ src/notes/classes/note.py
@@ -4,17 +4,17 @@ from datetime import datetime
 class Note:
     def __init__(self, name):
         self.name = name
-        self.createAt = datetime.now()
-        self.updatedAt = self.createAt
+        self.create_at = datetime.now()
+        self.updated_at = self.create_at
         self.content = ""
         self.tags = []
 
     def __str__(self):
-        return f"Note name: {self.name}, {self.updatedAt.strftime('%Y-%m-%d %H:%M:%S')} {self.content[:20]}..."
+        return f"Note name: {self.name}, {self.updated_at.strftime('%Y-%m-%d %H:%M:%S')} {self.content[:20]}..."
     
-    def setContent(self, content):
+    def set_content(self, content):
         self.content = content
-        self.updatedAt = datetime.now()
+        self.updated_at = datetime.now()
 
     def add_tag(self, tag: str):
         if tag not in self.tags:
