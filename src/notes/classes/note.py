594e5584de99853c659d3a7123bf3588e01c59c3
Natalia <icxodnik@gmail.com>
2025-03-29 03:07:40 +0100

    set default value for content

--- src/notes/classes/note.py
+++ src/notes/classes/note.py
@@ -14,18 +14,18 @@ class Note:
     def __str__(self):
 
         text = Text()
-        text.append(f"Note name: {self.name}", style="bold cyan")
+        text.append(f"Note name: {str(self.name)}", style="bold cyan")
         text.append(
             f"\nLast updated: {self.updated_at.strftime('%Y-%m-%d %H:%M:%S')}", style="italic")
         if self.tags:
             text.append("\nTags: ", style="bold")
             text.append(", ".join(self.tags), style="yellow")
         text.append("\nContent: \n", style="bold")
-        text.append(self.content, style="white")
+        text.append(str(self.content), style="white")
         return str(text)
 
     def set_content(self, content):
-        self.content = content
+        self.content = content if content is not None else ""
         self.updated_at = datetime.now()
 
     def add_tag(self, tag: str):
