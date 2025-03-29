2a9251e0c48b7bc7377ef1214bc8b4a8ac4309ef
Natalia Khodorova <icxodnik@gmail.com>
2025-03-29 03:54:47 +0100

    show AskScreen as proper modal window

--- src/notes/notes_ui.py
+++ src/notes/notes_ui.py
@@ -54,8 +54,36 @@ class EditorScreen(ModalScreen[str]):
         if event.button.id == "back":
             self.app.pop_screen()
 
-class AskScreen(ModalScreen[bool]):  
+class AskScreen(ModalScreen[bool]):
     """Screen with a dialog to ask a question."""
+    
+    CSS = """
+    AskScreen {
+        align: center middle;
+    }
+
+    #dialog {
+        grid-size: 2;
+        grid-gutter: 1 2;
+        grid-rows: 1fr 3;
+        padding: 0 1;
+        width: 40%;
+        height: 11;
+        border: thick $background 80%;
+        background: $surface;
+    }
+
+    #question {
+        column-span: 2;
+        height: 1fr;
+        width: 1fr;
+        content-align: center middle;
+    }
+
+    Button {
+        width: 100%;
+    }
+    """
     
     def __init__(self, question: str):
         super().__init__()
