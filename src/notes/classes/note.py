fc967b32fc3a55725d93027e13e42618d2d7bd42
Natalia Khodorova <icxodnik@gmail.com>
2025-03-29 15:51:52 +0100

    fix wrong PR #50 merge

--- src/notes/classes/note.py
+++ src/notes/classes/note.py
@@ -9,19 +9,6 @@ class Note:
         self.tags = []
 
 
-    def __str__(self):
-
-        text = Text()
-        text.append(f"Note name: {str(self.name)}", style="bold cyan")
-        text.append(
-            f"\nLast updated: {self.updated_at.strftime('%Y-%m-%d %H:%M:%S')}", style="italic")
-        if self.tags:
-            text.append("\nTags: ", style="bold")
-            text.append(", ".join(self.tags), style="yellow")
-        text.append("\nContent: \n", style="bold")
-        text.append(str(self.content), style="white")
-        return str(text)
-
     def set_content(self, content):
         self.content = content if content is not None else ""
         self.updated_at = datetime.now()
