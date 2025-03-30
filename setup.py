93a3e67486e50e160ffe36c49736ff13a16307cf
Filip <137682962+Filip-Povidernyi@users.noreply.github.com>
2025-03-30 13:18:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AttentionNotebookBot",
-    version="1.0.0",
+    version="1.0.1",
     authors=[
         {"name": "Natalia Khodorova", "email": "icxodnik@gmail.com"},
         {"name": "Leskovec Maksim", "email": "leskovecmaksim@gmail.com"},
