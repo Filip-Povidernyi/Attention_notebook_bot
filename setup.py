218631d9524450ca258efbf1798f5695255bd148
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-03-30 13:00:58 +0300

    change structure
    Переніс main.py у src/attentionnotebookbot/

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
-            "start-bot=main:main",
+            "start-bot=attentionnotebookbot.main:main",
         ],
     },
     classifiers=[
